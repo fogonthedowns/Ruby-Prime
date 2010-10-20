dde1efdccee9871c6819e587ba13fb8db213fb08
Justin Zollars <fogonthedowns@Macintosh-6.local>
2010-10-20 15:05:52 -0700

    comments

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
-state = Numeric.new
-print "2,3,"
+amiprime = Numeric.new
 (4..10000).each do
    |i|
    (2..(Math.sqrt(i).ceil)).each do
       |thing|
       state = 1
       if (i.divmod(thing)[1] == 0)
-         state = 0
+         amiprime = 0
          break
       end
    end
