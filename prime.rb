e957a563b6de4316e24c17d47c2a94dc12cbe270
Justin Zollars <fogonthedowns@gmail.com>
2011-04-06 22:37:18 -0700

    My Way included!

--- prime.rb
+++ prime.rb
@@ -1,3 +1,21 @@
+# my way
+def prime?(number)
+  prime_num = 0
+  a =[]
+  (2..(number-1)).each do |y|
+    a << number%y 
+    if a.include?(0)
+      prime_num = false
+    else 
+      prime_num = true
+    end
+  end
+  puts prime_num
+end
+
+
+
+# their way
 amiprime = Numeric.new
 (4..10000).each do
    |i|
